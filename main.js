e7990aea15b4db5333782fea51a62829f327f300
NaMillan <nazarethmillan@gmail.com>
2023-12-20 11:27:53 +0100

    add if

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 'use strict';
 const list = document.querySelector('.js-list');
-
+const input_search_desc = document.querySelector('.js_in_search_desc');
 const kittenOneImage = 'https://dev.adalab.es/gato-siames.webp';
 const kittenOneName = 'Anastacio'.toUpperCase();
 const kittenOneDesc = 'Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.';
@@ -16,7 +16,6 @@ const kittenThreeImage = 'https://dev.adalab.es/maine-coon-cat.webp';
 const kittenThreeName = 'Cielo' .toUpperCase();
 const kittenThreeDesc = 'Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.';
 const kittenThreeRace = 'Maine Coon';
-
 
 
 const kittenOne = `<li class="card">
@@ -55,5 +54,19 @@ const kittenThree = `<li class="card">
 <p class="card_description">${kittenThreeDesc}
 </p>
 </li>`;
-
 list.innerHTML = kittenOne + kittenTwo + kittenThree;
+
+const descrSearchText = input_search_desc.value;
+if( kittenOneDesc.includes(descrSearchText) ) {
+ list.innerHTML = kittenOne;   
+}
+
+if( kittenTwoDesc.includes(descrSearchText) ) {
+ list.innerHTML+= kittenTwo;   
+}
+
+if( kittenThreeDesc.includes(descrSearchText) ) {
+ list.innerHTML+= kittenThree;   
+}
+
+
