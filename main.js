75a80eb739e6db4c3c1a46d99a941d9db32edd68
Teresa Toledo <laratoledoteresa@hotmail.com>
2024-01-03 12:02:17 +0100

    Fix collapsed

--- main.js
+++ main.js
@@ -3,18 +3,20 @@ const list = document.querySelector('.js-list');
 const input_search_desc = document.querySelector('.js_in_search_desc');
 const kittenOneImage = 'https://dev.adalab.es/gato-siames.webp';
 const kittenOneName = 'Anastacio'.toUpperCase();
-const kittenOneDesc = 'Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.';
+const kittenOneDesc =
+  'Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.';
 const kittenOneRace = 'Siamés';
-
 
 const kittenTwoImage = 'https://dev.adalab.es/sphynx-gato.webp';
 const kittenTwoName = 'Fiona'.toUpperCase();
-const kittenTwoDesc = 'Produce fascinación y curiosidad. Exótico, raro, bello, extraño hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.';
+const kittenTwoDesc =
+  'Produce fascinación y curiosidad. Exótico, raro, bello, extraño hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.';
 const kittenTwoRace = 'Sphynx';
 
 const kittenThreeImage = 'https://dev.adalab.es/maine-coon-cat.webp';
-const kittenThreeName = 'Cielo' .toUpperCase();
-const kittenThreeDesc = 'Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.';
+const kittenThreeName = 'Cielo'.toUpperCase();
+const kittenThreeDesc =
+  'Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.';
 const kittenThreeRace = 'Maine Coon';
 
 const addBtn = document.querySelector('.js-btn-add');
@@ -25,61 +27,53 @@ const labelMessageError = document.querySelector('.js-label-error');
 const cancelBtn = document.querySelector('.js-btn-cancel');
 const newForm = document.querySelector('.js-new-form');
 
-
-
-//1LLAMAMOS AL SIMBOLO MAS 
+//1LLAMAMOS AL SIMBOLO MAS
 const addPlus = document.querySelector('.js-add');
 
 //4 CREAMOS LA FUNCION MANEJADORA E INCLUIMOS LAS FUNCIONES DEL PUNTO 3
 function handleClickNewCatForm(event) {
-    event.preventDefault();
-    if (newForm.classList.contains('collapsed')) {
-        showNewCatForm();
-        
-    } else {
-        hideNewCatForm();
-        
-    }
+  event.preventDefault();
+  if (newForm.classList.contains('collapsed')) {
+    showNewCatForm();
+  } else {
+    hideNewCatForm();
   }
-//3 CREAMOS LAS FUNCIONES 
-  function showNewCatForm() {
-    newForm.classList.remove('collapsed');
-  }
-  function hideNewCatForm() {
-    newForm.classList.add('collapsed');
-  }
- 
-  
+}
+//3 CREAMOS LAS FUNCIONES
+function showNewCatForm() {
+  newForm.classList.remove('collapsed');
+}
+function hideNewCatForm() {
+  newForm.classList.add('collapsed');
+}
+
 //2 LLAMAMOS AL EVENTO SOBRE EL +
-  addPlus.addEventListener('click',handleClickNewCatForm);
+addPlus.addEventListener('click', handleClickNewCatForm);
 
-  //PUNTO 2. HEMOS LA FUNCION ADDNEWKITTEN Y HEMOS METIDO LAS CONSTANTES DEL EVENTO ADDBTN
-  
-  function addNewKitten(event) {
-    const valueDesc = inputDesc.value;
-    const valuePhoto = inputPhoto.value;
-    const valueName = inputName.value;
-    if (valueDesc === '' || valuePhoto === '' || valueName === '') {
-        labelMessageError.innerHTML = '¡Uy! parece que has olvidado algo.'
-    }
+//PUNTO 2. HEMOS LA FUNCION ADDNEWKITTEN Y HEMOS METIDO LAS CONSTANTES DEL EVENTO ADDBTN
+
+function addNewKitten(event) {
+  const valueDesc = inputDesc.value;
+  const valuePhoto = inputPhoto.value;
+  const valueName = inputName.value;
+  if (valueDesc === '' || valuePhoto === '' || valueName === '') {
+    labelMessageError.innerHTML = '¡Uy! parece que has olvidado algo.';
   }
+}
 
+addBtn.addEventListener('click', addNewKitten);
 
-addBtn.addEventListener('click',addNewKitten);
-    
-
-
-cancelBtn.addEventListener('click', (event)=>{
-    inputPhoto.value = '';
-    inputDesc.value = '';
-    inputName.value = '';
-    newForm.classList.add('collapsed');
+cancelBtn.addEventListener('click', (event) => {
+  inputPhoto.value = '';
+  inputDesc.value = '';
+  inputName.value = '';
+  newForm.classList.add('collapsed');
 });
 
-//PUNTO 3 HEMOS CREADO UNA FUNCION RENDERKITTEN PARA UNIFICAR EL CODIGO PARA CREAR UN GATO 
+//PUNTO 3 HEMOS CREADO UNA FUNCION RENDERKITTEN PARA UNIFICAR EL CODIGO PARA CREAR UN GATO
 
 function renderKitten(url, desc, name, race) {
-    return `<li class="card">
+  return `<li class="card">
 <img
     class="card_img"
     src="${url}"
@@ -90,31 +84,40 @@ function renderKitten(url, desc, name, race) {
 <p class="card_description">${desc}
 </p>
 </li>`;
-  }
-  
+}
 
+const kittenOne = renderKitten(
+  kittenOneImage,
+  kittenOneDesc,
+  kittenOneName,
+  kittenOneRace
+);
 
-const kittenOne = renderKitten(kittenOneImage, kittenOneDesc, kittenOneName, kittenOneRace);
+const kittenTwo = renderKitten(
+  kittenTwoImage,
+  kittenTwoDesc,
+  kittenTwoName,
+  kittenTwoRace
+);
 
-const kittenTwo = renderKitten(kittenTwoImage, kittenTwoDesc, kittenTwoName, kittenTwoRace);
-
-const kittenThree = renderKitten(kittenThreeImage, kittenThreeDesc, kittenThreeName, kittenThreeRace);
+const kittenThree = renderKitten(
+  kittenThreeImage,
+  kittenThreeDesc,
+  kittenThreeName,
+  kittenThreeRace
+);
 
 list.innerHTML = kittenOne + kittenTwo + kittenThree;
 
-
-
 const descrSearchText = input_search_desc.value;
-if( kittenOneDesc.includes(descrSearchText) ) {
- list.innerHTML = kittenOne;   
+if (kittenOneDesc.includes(descrSearchText)) {
+  list.innerHTML = kittenOne;
 }
 
-if( kittenTwoDesc.includes(descrSearchText) ) {
- list.innerHTML+= kittenTwo;   
+if (kittenTwoDesc.includes(descrSearchText)) {
+  list.innerHTML += kittenTwo;
 }
 
-if( kittenThreeDesc.includes(descrSearchText) ) {
- list.innerHTML+= kittenThree;   
+if (kittenThreeDesc.includes(descrSearchText)) {
+  list.innerHTML += kittenThree;
 }
-
-
